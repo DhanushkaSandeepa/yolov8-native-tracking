92ed65a7f168c1dbf1ce0394e07332fd5c947aa6
DhanushkaSandeepa <142682689+DhanushkaSandeepa@users.noreply.github.com>
2023-08-19 21:56:18 +0530

    YOLOv8 Tracker for a single class.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import cv2
-
 from ultralytics import YOLO
 import supervision as sv
 import numpy as np
 
-
-LINE_START = sv.Point(320, 0)
-LINE_END = sv.Point(320, 480)
-
+LINE_START = sv.Point(0, 360)
+LINE_END = sv.Point(1280, 360)
 
 def main():
     line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
@@ -18,36 +15,41 @@ def main():
         text_scale=0.5
     )
 
-    model = YOLO("yolov8l.pt")
-    for result in model.track(source=0, show=True, stream=True, agnostic_nms=True):
-        
+    model = YOLO("YOUR/MODEL")
+    for result in model.track(source="YOUR/VIDEO/PATH", show=True, stream=True, agnostic_nms=True):
+
         frame = result.orig_img
         detections = sv.Detections.from_yolov8(result)
 
         if result.boxes.id is not None:
             detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
-        
-        detections = detections[(detections.class_id != 60) & (detections.class_id != 0)]
 
-        labels = [
-            f"{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
-            for _, confidence, class_id, tracker_id
-            in detections
-        ]
+        valid_indices = np.where(detections.class_id == 0)[0]
+        valid_detections = detections[valid_indices]
 
-        frame = box_annotator.annotate(
-            scene=frame, 
-            detections=detections,
-            labels=labels
-        )
+        for detection in valid_detections:
+            tracker_id = detection[4]
+            class_id = 0  # Assuming the class ID is always 0
+            confidence = detection[2]
 
-        line_counter.trigger(detections=detections)
-        line_annotator.annotate(frame=frame, line_counter=line_counter)
+            labels = [
+                f"{tracker_id} {class_id} {confidence:0.2f}"
+                for _,_, confidence, class_id, tracker_id
+                in detections
+            ]
+            frame = box_annotator.annotate(
+                scene=frame,
+                detections=detections,
+                labels=labels
+            )
 
-        cv2.imshow("yolov8", frame)
+            line_counter.trigger(detections=detections)  # Assuming bounding box coordinates are at index 2
+            line_annotator.annotate(frame=frame, line_counter=line_counter)
 
-        if (cv2.waitKey(30) == 27):
-            break
+            cv2.imshow("yolov8", frame)
+
+            if (cv2.waitKey(30) == 27):
+                break
 
 
 if __name__ == "__main__":
